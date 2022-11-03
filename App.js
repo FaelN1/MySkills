602c0808613ed97310f425b484074c1142e8e040
Rafael Nicolas <nicolasrafael178@gmail.com>
2022-11-02 21:29:08 -0300

    Inicio Do Projeto

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function myskills() {
+ return (
+   <View>
+    <Text>
+      Incio do Projeto
+    </Text>
+   </View>
+  );
+}
